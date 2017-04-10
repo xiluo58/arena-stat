9bc9dfabe1eecde1922c593c6f66b9aef9c28fbc
Xi Luo <luoxi58@gmail.com>
2017-04-10 08:19:16 +0800

    Always send authorization if there is a token

--- src/app/classes/base-api.ts
+++ src/app/classes/base-api.ts
@@ -5,20 +5,33 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
+import { AccountService } from '../services/account.service';
+
 export class BaseAPI {
   static injector: Injector;
 
   protected apiUrlPrefix: string;
   private http: Http;
+  private accountService: AccountService;
 
-  constructor(
-  ) {
+  constructor() {
     this.apiUrlPrefix = BaseAPI.injector.get(ConfigService).getSettings('apiUrlPrefix');
     this.http = BaseAPI.injector.get(Http);
+    this.accountService = BaseAPI.injector.get(AccountService);
   }
 
   getBodyObject(res) {
     return res.json() || {};
+  }
+
+  getDefaultHeaders(): Headers {
+    const headers = new Headers();
+    headers.set('content-type', 'application/json');
+    const token = this.accountService.getToken();
+    if (token) {
+      headers.set('Authorization', 'Bearer ' + token);
+    }
+    return headers;
   }
 
   post(url, params, options?) {
@@ -28,14 +41,7 @@ export class BaseAPI {
     } else {
       postBody = JSON.stringify(params);
     }
-    const headers = new Headers();
-    headers.set('content-type', 'application/json');
-    if (options && options.token) {
-      headers.set('Authorization', 'Bearer ' + options.token);
-    }
-    /* const requestOptions = new RequestOptions({
-      headers: headers
-    }); */
+    const headers = this.getDefaultHeaders();
     return this.http.post(this.apiUrlPrefix + url, postBody, {headers: headers}).map(
       res => {
         return this.getBodyObject(res);
@@ -48,7 +54,8 @@ export class BaseAPI {
   }
 
   get(url) {
-    return this.http.get(this.apiUrlPrefix + url).map(
+    const headers = this.getDefaultHeaders();
+    return this.http.get(this.apiUrlPrefix + url, {headers: headers}).map(
       res => {
         return this.getBodyObject(res);
       },
